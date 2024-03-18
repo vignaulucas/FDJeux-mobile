f00d4346f0d58c45b743fe52afbf1d19aa5a936a
Lucas Vignau <lucas.vignau@etu.umontpellier.fr>
2024-03-18 17:57:31 +0100

    Initial Commit

---
+++ FDJeux mobile/FDJeux_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  FDJeux_mobileApp.swift
+//  FDJeux mobile
+//
+//  Created by VIGNAU Lucas on 18/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct FDJeux_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FDJeux mobileUITests/FDJeux_mobileUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FDJeux_mobileUITestsLaunchTests.swift
+//  FDJeux mobileUITests
+//
+//  Created by VIGNAU Lucas on 18/03/2024.
+//
+
+import XCTest
+
+final class FDJeux_mobileUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
